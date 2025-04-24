40b2ae3f210d2499813b3fa70cd72cd1bc94a627
Joeri Malmberg <joeri.malmberg@blackbird.cloud>
2025-04-24 15:17:40 +0200

    Removed route53 resolvers Removed security groups

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,4 @@
-output "security_group" {
-  description = "The AWS security group used to controll ingress traffic to the Client VPN self-service-portal."
-  value       = module.sg
-}
-
 output "vpn" {
   description = "The Client VPN endpoint."
   value       = aws_ec2_client_vpn_endpoint.vpn
-}
-
-output "resolver_security_group" {
-  description = "The AWS security group used to controll ingress traffic to the Route 53 DNS resolver endpoint."
-  value       = module.resolver_sg
-}
-
-output "aws_route53_resolver_endpoint" {
-  description = "The Route53 DNS resolver endpoint."
-  value       = aws_route53_resolver_endpoint.vpn_dns
 }
--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4"
+      version = ">= 5"
     }
   }
-  required_version = ">= 1"
+  required_version = ">= 1.5"
 }
